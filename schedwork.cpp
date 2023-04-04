0c198e8443761c95d26b3d3186ecffe655182eba
Xichen Gao <xichenga@usc.edu>
2023-04-04 12:01:46 +0000

    finished

--- schedwork.cpp
+++ schedwork.cpp
@@ -20,7 +20,7 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
-
+bool scheduleHelper(const AvailabilityMatrix& avail, const size_t dailyNeed, const size_t maxShifts, DailySchedule& sched, map<int,int>& mapShifts, int row, int col);
 
 // Add your implementation of schedule() and other helper functions here
 
@@ -37,8 +37,46 @@ bool schedule(
     sched.clear();
     // Add your code below
 
+		map<int, int> mapShifts;
+		for(int i = 0; i < avail[0].size(); i++){
+			mapShifts[i] = 0;
+		}
+		sched.resize(avail.size());
+		for(int i = 0; i < sched.size(); i++){
+			sched[i].resize(dailyNeed);
+			for(int j = 0; j < dailyNeed; j++){
+				sched[i][j] = -1;
+			}
+		}
+    return scheduleHelper(avail, dailyNeed, maxShifts, sched, mapShifts, 0, 0);
+    
+
+}
 
 
-
+bool scheduleHelper(const AvailabilityMatrix& avail, const size_t dailyNeed, const size_t maxShifts, DailySchedule& sched, map<int,int>& mapShifts, int row, int col){
+    if(row==sched.size()){
+        return true;
+    }
+    if(col==dailyNeed){
+        return scheduleHelper(avail,dailyNeed,maxShifts,sched,mapShifts,row+1,0);
+    }
+		if(sched[row][col]==-1){
+			for(int i = 0; i < avail[0].size(); i++){
+				 bool notFound = true;
+				 if(find(sched[row].begin(),sched[row].end(),i)!=sched[row].end()) notFound = false;
+        sched[row][col] = i;
+				mapShifts[i]++;
+        if(avail[row][i]==1 && mapShifts[i]<=maxShifts && notFound){
+            bool status = scheduleHelper(avail,dailyNeed,maxShifts,sched,mapShifts,row, col+1);
+            if(status) return true;
+        }
+				mapShifts[i]--;
+				
+    	}
+			sched[row][col] = -1;
+			return false;
+		}
+    else return scheduleHelper(avail,dailyNeed,maxShifts,sched,mapShifts,row,col+1);
 }
 
--- wordle.cpp
+++ wordle.cpp
@@ -13,7 +13,7 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
-
+void wordleHelper(string& in, std::string& tempFloating, const std::string& floating, const std::set<std::string>& dict, int i, std::set<std::string>& res, int numberDashes);
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -22,7 +22,72 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
-
+	std::set<std::string> res;
+	string temp = in;
+	string tempFloating = floating;
+	int numberDashes = 0;
+	for(int i = 0; i < temp.size(); i++){
+		if(temp[i]=='-') numberDashes++;
+	}
+	wordleHelper(temp, tempFloating, floating, dict, 0, res, numberDashes);
+	return res;
 }
 
 // Define any helper functions here
+void wordleHelper(string& in, std::string& tempFloating, const std::string& floating, const std::set<std::string>& dict, int i, std::set<std::string>& res, int numberDashes){
+	if(i==in.length()){
+		if(dict.find(in)!=dict.end()) {
+			res.insert(in);
+			//cout<<in<<endl;
+		}
+		return;
+	}
+
+	if(in[i] == '-'){
+		//if # '-' != number of floating left
+		//cout << numberDashes << endl;
+			if(numberDashes!=tempFloating.size()){
+				for(int j = 0; j < 26; j++){
+					in[i] = 'a'+j;
+					//if a+j is afloating, remove it
+					bool wasFloating = false;
+					if(tempFloating.find('a'+j)!=string::npos) {
+						tempFloating.erase(tempFloating.find('a'+j),1);
+						wasFloating = true;
+						//in[i]='-';
+						//numberDashes++;
+					}
+					numberDashes--;
+					
+					wordleHelper(in, tempFloating, floating, dict, i+1, res, numberDashes);
+					numberDashes++;
+					//string temp = in[i];
+					in[i] = '-';
+					//if a+j was floating, reinsert it;
+					if(wasFloating) {
+						//tempFloating.insert(floating.find('a'+j), to_string(tempChar));
+						tempFloating+='a'+j;
+					}
+				}
+			}
+			else{
+				//cout << 0000000000000000<< endl;
+				
+				for(int j = 0; j < tempFloating.size(); j++){
+						in[i] = tempFloating[j];
+						char tempChar = tempFloating[j];
+						int pos = tempFloating.find(tempChar);
+						string tempString = tempFloating;
+						tempFloating = tempFloating.erase(tempFloating.find(tempChar),1);
+						numberDashes--;
+						wordleHelper(in, tempFloating, floating, dict, i+1, res, numberDashes);
+						numberDashes++;
+						in[i]='-';
+						tempFloating = tempString;
+				}
+				//tempFloating = floating;
+			}
+				//in[i] = '-';
+	}
+	else wordleHelper(in, tempFloating, floating, dict, i+1, res, numberDashes);
+}
